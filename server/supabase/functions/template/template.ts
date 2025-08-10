a6aa2b2fb9fb3d2cf50af4e7461ed05a1ea4a3ce
axxy <73447957+axxish@users.noreply.github.com>
2025-08-11 01:41:10 +0300

    test

--- server/supabase/functions/template/template.ts
+++
@@ -1,8 +0,0 @@
-import {
-  errorResponse,
-} from '../_shared/util.ts'
-
-
-export async function fnName(req: Request): Promise<Response> {
-  return errorResponse(400, 'This is just a template function');
-}
